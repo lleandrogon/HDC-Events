541b31235d6d030006ece701e9d20ee513e7a724
Leandro Gonçalves <lleandrogon2004@gmail.com>
2025-04-01 13:39:55 -0300

    Submit de imagens

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -4,30 +4,44 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Event;
 
 class EventController extends Controller
 {
     public function index() {
-        $nome = "Matheus";
-        $idade = 29;
+        $events = Event::all();
 
-        $arr = [10, 20, 30, 40, 50];
-
-        $nomes = ["Matheus", "Maria", "João", "Saulo"];
-
-        return view(
-            'welcome',
-            [
-                'nome' => $nome,
-                'idade' => $idade,
-                'profissao' => 'Programador',
-                'arr' => $arr,
-                'nomes' => $nomes
-            ]
-        );
+        return view('welcome', ['events' => $events]);
     }
 
     public function create() {
         return view('events.create');
+    }
+
+    public function store(Request $request) {
+        $event = new Event();
+        
+        $event->title = $request->title;
+        $event->city = $request->city;
+        $event->private = $request->private;
+        $event->description = $request->description;
+
+        // Image Upload
+        if($request->hasFile('image') && $request->file('image')->isValid()) {
+
+            $requestImage = $request->image;
+
+            $extension = $requestImage->extension();
+
+            $imageName = md5($requestImage->getClientOriginalName() . strtotime("now")) . "." . $extension;
+
+            $request->image->move(public_path('img/events'), $imageName);
+
+            $event->image = $imageName;
+        }
+
+        $event->save();
+
+        return redirect('/')->with('msg', 'Evento criado com sucesso');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -7,16 +7,8 @@ Route::get('/', [EventController::class, 'index']);
 
 Route::get('/events/create', [EventController::class, 'create']);
 
+Route::post('/events', [EventController::class, 'store']);
+
 Route::get('/contact', function () {
     return view('contact');
-});
-
-Route::get('/produtos', function () {
-    $busca = request('search');
-
-    return view('products', ['busca' => $busca]);
-});
-
-Route::get('/produtos_teste/{id}', function ($id = null) {
-    return view('product', ['id' => $id]);
 });
