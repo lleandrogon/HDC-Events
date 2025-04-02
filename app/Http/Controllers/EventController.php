5e4d80a1ce249e74bacd666f0d38bdd9ed88bdd4
Leandro Gonçalves <lleandrogon2004@gmail.com>
2025-04-02 13:30:19 -0300

    Adicionando autenticação

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -9,9 +9,21 @@ use App\Models\Event;
 class EventController extends Controller
 {
     public function index() {
-        $events = Event::all();
+        $search = request('search');
 
-        return view('welcome', ['events' => $events]);
+        if ($search) {
+            
+            $events = Event::where([
+                ['title', 'like', '%'.$search.'%']
+            ])->get();
+
+        } else {
+
+            $events = Event::all();
+
+        }
+
+        return view('welcome', ['events' => $events, 'search' => $search]);
     }
 
     public function create() {
@@ -22,9 +34,11 @@ class EventController extends Controller
         $event = new Event();
         
         $event->title = $request->title;
+        $event->date = $request->date;
         $event->city = $request->city;
         $event->private = $request->private;
         $event->description = $request->description;
+        $event->items = $request->items;
 
         // Image Upload
         if($request->hasFile('image') && $request->file('image')->isValid()) {
@@ -43,5 +57,11 @@ class EventController extends Controller
         $event->save();
 
         return redirect('/')->with('msg', 'Evento criado com sucesso');
+    }
+
+    public function show($id) {
+        $event = Event::findOrFail($id);
+
+        return view('events.show', ['event' => $event]);
     }
 }
--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -17,6 +17,10 @@
       <input type="text" class="form-control" id="title" name="title" placeholder="Nome do evento">
     </div>
     <div class="form-group mb-3">
+      <label for="date">Data do evento:</label>
+      <input type="date" class="form-control" id="date" name="date">
+    </div>
+    <div class="form-group mb-3">
       <label for="title">Cidade:</label>
       <input type="text" class="form-control" id="city" name="city" placeholder="Local do evento">
     </div>
@@ -30,6 +34,24 @@
     <div class="form-group mb-3">
       <label for="title">Descrição:</label>
       <textarea name="description" id="description" class="form-control" placeholder="O que vai acontecer no evento?"></textarea>
+    </div>
+    <div class="form-group mb-3">
+      <label for="title">Adicione item de infraestrutura:</label>
+      <div class="form-group">
+        <input type="checkbox" name="items[]" value="Cadeiras"> Cadeiras
+      </div>
+      <div class="form-group">
+        <input type="checkbox" name="items[]" value="Palco"> Palco
+      </div>
+      <div class="form-group">
+        <input type="checkbox" name="items[]" value="Cerveja grátis"> Cerveja grátis
+      </div>
+      <div class="form-group">
+        <input type="checkbox" name="items[]" value="Open Food"> Open food
+      </div>
+      <div class="form-group">
+        <input type="checkbox" name="items[]" value="Brindes"> Brindes
+      </div>
     </div>
     <input type="submit" class="btn btn-primary" value="Criar Evento">
   </form>
